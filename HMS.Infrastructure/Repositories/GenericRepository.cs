8c841d8b9ef661775743a9dd331f00191c99036b
iiahmed04 <ahmedziadan2906@gmail.com>
2026-03-16 22:59:50 +0200

    GenericRepository and UnitOfWork setup

---
+++ HMS.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using HMS.Core.Contracts;
+using HMS.Core.Entities;
+using HMS.Infrastructure.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace HMS.Infrastructure.Repositories
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly HotelDbContext _dbContext;
+
+        public GenericRepository(HotelDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+            => await _dbContext.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)
+            => await _dbContext.Set<TEntity>().FindAsync(id);
+
+        public void Update(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+        }
+    }
+}
